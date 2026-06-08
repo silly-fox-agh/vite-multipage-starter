5624b5c437a4e2b64f4d496dbab62eb7ad603116
silly-fox-agh <814magdalena814@gmail.com>
2026-06-09 01:57:28 +0200

    Konfiguracja Vite

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,16 @@ import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  base: '/vite-multipage-starter/',
+  base: '/vite-multipage-starter/', 
   plugins: [
     tailwindcss(),
   ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        login: 'login.html'
+      }
+    }
+  }
 });
